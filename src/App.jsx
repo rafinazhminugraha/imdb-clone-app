128e250f1ca0f9f87851fd9240cf4436fa72bd97
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-04-03 00:48:38 +0700

    progressing the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
+  const [searchTerm, setsearchTerm] = useState('Avengers');
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern" />
+      
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find All <span className='text-gradient'>Movies</span> Without Wasting Your Time
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const search = ({searchTerm , setSearchTerm}) => {
+  return (
+    <div className='text-white text-3xl'>{searchTerm}</div>
+  )
+}
+
+export default search
