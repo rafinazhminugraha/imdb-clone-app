447f7c496ffa9267a6fe3fa20c4b761a5e7700ad
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-04-12 19:57:23 +0700

    add the loading animation and finishing the movie lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Search from './components/Search.jsx';
+import Spinner from './components/spinner';
 
 const API_BASE_KEY = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -65,10 +66,10 @@ const App = () => {
         </header>
 
         <section className='all-movies'>
-          <h2>All Movies</h2>
+          <h2 className='mt-[40px]'>All Movies</h2>
 
           {isLoading ? (
-            <p className='text-white'>Loading...</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className='text-red-500'>{errorMessage}</p>
           ) : (
