b2d0c70015b68825baf890dd430f1ed88845a1cc
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-04-13 23:23:15 +0700

    finishing the font issue and setup appwrite database

---
+++ src/appwrite.js
@@ -0,0 +1,6 @@
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+console.log(PROJECT_ID,DATABASE_ID,COLLECTION_ID);
+
